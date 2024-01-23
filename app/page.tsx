51132327adf67b03265794e52ab65da78e70ff8b
Parth Madhvani <madhvaniparth2@gmail.com>
2024-01-24 03:02:32 +0530

    building hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
       <div>
         <div className="overflow-hidden">
-          <div className="relative h-screen">
+          <div className="relative">
             <img className='absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image' width='4377' src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" alt="" />
           </div>
           <div className="Hero-section px-3">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'ssm' : '537px',
+        'sssm' : '380px'  
+      }
     },
   },
   plugins: [],
